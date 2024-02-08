c038ed6ff5f76a7432bb865931106a1f08e340ab
French <1078924850117002@mil>
2024-02-08 14:00:08 +0900

    Finished Accordion

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,37 +1,31 @@
-import { GoBell, GoGlobe, GoCopy } from "react-icons/go";
-import Button from "./components/Button";
+import Accordion from "./components/Accordion";
 
 function App() {
+  const items = [
+    {
+      id: 1,
+      label: 'Alright, lets mafia things up a bit',
+      content: 'But I know you in the future. I cleaned your poop. Yes, except the Dave Matthews Band does not rock. Now what? I do not want to be rescued.',
+    },
 
+    {
+      id: 3,
+      label: 'Hi, I am a naughty nurse',
+      content: 'Would you censor the Venus de Venus just because you can see her spewers?'
+    },
+
+    {
+      id: 2,
+      label: 'On second thoughts, let\'s not go there. It is a silly place.',
+      content: 'He hasn\'t got shit all over him. Now, look here, my good man. Well, I got better. Found them?'
+    }
+
+  ];
 
   return (
     <>
       <div>
-        <Button secondary rounded>
-          <GoBell />
-          Secondary
-        </Button>
-      </div>
-      <div>
-        <Button success>
-          <GoCopy/>
-          Success
-        </Button>
-      </div>
-      <div>
-        <Button danger>Danger</Button>
-      </div>
-      <div>
-        <Button warning>Warning</Button>
-      </div>
-      <div>
-        <Button danger rounded>
-          <GoGlobe/>
-          Errything
-        </Button>
-      </div>
-      <div>
-        <Button primary>Primary</Button>
+        <Accordion items={ items } />
       </div>
     </>
   );
